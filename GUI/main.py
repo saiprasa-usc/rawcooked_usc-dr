849fa323dd3734e645623f98a88d1d7fb2658ceb
trinonandi <trino.nandi@gmail.com>
2024-03-22 13:12:49 -0700

    Add browse folder feature

--- GUI/main.py
+++ GUI/main.py
@@ -1,19 +1,56 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-
 import sys
 
+from PyQt5.QtWidgets import (
+    QLabel, QApplication, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QWidget, QFormLayout, QFileDialog
+)
 
-class MainWindow(QMainWindow):
+
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        # Class Level UI Elements
+        self.base_form_layout = None
+        self.root_path_browse_button = None
+        self.root_path_line_edit = None
+        self.base_layout = None
+
+        # Class Level Data Variables
+        self.root_path = None
+
+        # Main window properties
         self.setWindowTitle("RawCooked Executor")
+        self.setGeometry(0, 0, 800, 600)
 
-        label = QLabel("Testing")
-        label.setAlignment(Qt.AlignCenter)
+        self.inti_ui()
 
-        self.setCentralWidget(label)
+    def inti_ui(self):
+        # Base Layouts
+        self.base_layout = QVBoxLayout()
+        self.base_form_layout = QFormLayout()
+
+        # Browse Root Path UI
+        hbox = QHBoxLayout()
+        self.root_path_line_edit = QLineEdit()
+        self.root_path_line_edit.setReadOnly(True)
+        root_path_label = QLabel("Path to Working Directory: ")
+        self.root_path_browse_button = QPushButton("Browse")
+        self.root_path_browse_button.clicked.connect(self.launch_root_path_browse_window)
+        hbox.addWidget(self.root_path_line_edit)
+        hbox.addWidget(self.root_path_browse_button)
+        self.base_form_layout.addRow(root_path_label, hbox)
+        self.base_layout.addLayout(self.base_form_layout)
+
+        self.setLayout(self.base_layout)
+
+    def launch_root_path_browse_window(self):
+        response = QFileDialog.getExistingDirectory(
+            self,
+            "Select a Folder",
+            "/home",
+            QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks,
+        )
+        self.root_path = response
 
 
 if __name__ == "__main__":
